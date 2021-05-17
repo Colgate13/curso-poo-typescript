808d8d6f12e48bc174e75100c01939af061f52da
Carlos Roberto <crpu4a@gmail.com>
2021-05-17 10:13:22 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
-import { Mage, Priest, Warrior } from "./Classes";
-import Personagem from "./Personagem";
+import { Mage, Priest, Warrior } from './Classes'
+import Personagem from './Personagem'
 
 let mage: Personagem = new Mage('Alakazan')
 let warrior: Personagem = new Warrior('Fuzarnin')
 let priest: Personagem = new Priest('Curander')
 
-console.log('mage :>> ', mage);
-console.log('warrior :>> ', warrior);
-console.log('priest :>> ', priest);
+console.log('mage :>> ', mage)
+console.log('warrior :>> ', warrior)
+console.log('priest :>> ', priest)
